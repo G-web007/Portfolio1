18f6e44f27b53737e57dcd8ef3d846a38129b218
Vincent Ygbuhay <ygbuhay.vincent@yahoo.com>
2025-06-14 10:05:44 +0800

    add HTML Bookmarks, Media Queries for home image, wrapper grid, javascript media queries for navbar

--- js/main.js
+++ js/main.js
@@ -1,12 +1,18 @@
+// Open Navbar
 const openBtn = document.querySelector('#openBtn');
-openBtn.addEventListener('click', openNav);
+openBtn.addEventListener('click', function() {
+const mediaQueries = window.matchMedia("(max-width: 992px)")
 
+  if(mediaQueries.matches) {
+    document.getElementById('myNav').style.width = '100%';
+  }else {
+    document.getElementById('myNav').style.width = '60%';
+  }
+});
+
+// Close Navbar
 const closeBtn = document.querySelector('#closeBtn');
 closeBtn.addEventListener('click', closeNav);
-
-function openNav() {
-  document.getElementById('myNav').style.width = '60%';
-}
 
 function closeNav() {
   document.getElementById('myNav').style.width = '0%';
